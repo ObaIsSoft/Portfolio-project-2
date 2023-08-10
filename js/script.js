62f88b1c711f6b75ea847fd7b254dcf05e205e12
jolaolu <adebayoanjola@gmail.com>
2023-08-10 21:57:39 +0100

    update javascript

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log( 'its working' )
 
-let themeDots = document.getElementByClassName( 'theme-dot' )
+let themeDots = document.getElementsByClassName('theme-dot' )
 
 for ( var i = 0; themeDots.length > i; i++ ) {
     themeDots[ i ].addEventListener( 'click', function () {
